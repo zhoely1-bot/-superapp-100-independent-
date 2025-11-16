9c0eadf0cf74be96d69de58386e0ac99a4313252
zhoely1 <zhoely1@example.com>
2025-11-16 20:43:06 +0100

    Configure SDK/NDK paths and fix Android build

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+import { startWSServer } from './src/services/websocket-server';
+import { initDB } from './src/services/db';
+
+// Lance la BDD + le serveur WebSocket
+initDB().then(() => {
+  startWSServer();
+});
